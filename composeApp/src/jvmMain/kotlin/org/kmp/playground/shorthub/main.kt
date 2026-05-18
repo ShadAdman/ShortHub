fb4dae43e67d9e6efce34960898ab04611cb59bf
shadmanadman <adman.shadman@gmail.com>
2026-05-18 11:41:41 +0100

    Introduce pref scene

--- composeApp/src/jvmMain/kotlin/org/kmp/playground/shorthub/main.kt
+++ composeApp/src/jvmMain/kotlin/org/kmp/playground/shorthub/main.kt
@@ -2,12 +2,21 @@ package org.kmp.playground.shorthub
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.kmp.playground.shorthub.db.di.dbModule
+import org.kmp.playground.shorthub.hub.di.hubModule
+import org.kmp.playground.shorthub.pref.di.prefModule
+import org.koin.core.context.startKoin
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "ShortHub",
-    ) {
-        App()
+fun main() {
+    startKoin {
+        modules(dbModule, hubModule, prefModule)
+    }
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "ShortHub",
+        ) {
+            App()
+        }
     }
 }
